d5a60f911357f9c899dedadc5f09d28dba7912c0
Diego Chen <c2535069428@gmail.com>
2026-03-15 16:43:07 +0100

    Sistema UI （menu）

--- Assets/UI/Scripts/ResultManager.cs
+++ Assets/UI/Scripts/ResultManager.cs
@@ -1,34 +1,65 @@
-using TMPro;
+//using TMPro;
+//using UnityEngine;
+
+//public class ResultManager : MonoBehaviour
+//{
+//    public TMP_Text timeText;
+//    public TMP_Text hitText;
+//    public TMP_Text hintText;
+
+//    private float finishTime;
+//    private int hitCount;
+//    private int hintCount;
+
+//    public void SetResults(float time, int hits, int hints)
+//    {
+//        finishTime = time;
+//        hitCount = hits;
+//        hintCount = hints;
+
+//        UpdateUI();
+//    }
+
+//    public void UpdateUI()
+//    {
+//        if (timeText != null)
+//            timeText.text = "完成时间: " + finishTime.ToString("F1") + " 秒";
+
+//        if (hitText != null)
+//            hitText.text = "碰撞次数: " + hitCount;
+
+//        if (hintText != null)
+//            hintText.text = "提示触发次数: " + hintCount;
+//    }
+//}
+
 using UnityEngine;
 
 public class ResultManager : MonoBehaviour
 {
-    public TMP_Text timeText;
-    public TMP_Text hitText;
-    public TMP_Text hintText;
+    public GameObject resultPanel;
 
-    private float finishTime;
-    private int hitCount;
-    private int hintCount;
-
-    public void SetResults(float time, int hits, int hints)
+    private void Start()
     {
-        finishTime = time;
-        hitCount = hits;
-        hintCount = hints;
-
-        UpdateUI();
+        if (resultPanel != null)
+        {
+            resultPanel.SetActive(false);
+        }
     }
 
-    public void UpdateUI()
+    public void ShowResultPanel()
     {
-        if (timeText != null)
-            timeText.text = "完成时间: " + finishTime.ToString("F1") + " 秒";
+        if (resultPanel != null)
+        {
+            resultPanel.SetActive(true);
+        }
+    }
 
-        if (hitText != null)
-            hitText.text = "碰撞次数: " + hitCount;
-
-        if (hintText != null)
-            hintText.text = "提示触发次数: " + hintCount;
+    public void HideResultPanel()
+    {
+        if (resultPanel != null)
+        {
+            resultPanel.SetActive(false);
+        }
     }
 }
