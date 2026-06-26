6e0d454df2c3abc9aeb4b9eac4f3417482b84a02
Diego Chen <c2535069428@gmail.com>
2026-06-26 18:34:29 +0200

    受击音效，小垃圾

--- Assets/Scripts/SceneLogic/obstáculo/DamageObject.cs
+++ Assets/Scripts/SceneLogic/obstáculo/DamageObject.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class DamageObject : MonoBehaviour
 {
+    private const string DefaultHitClipPath = "Assets/UI/Audio/shouji.mp3";
+
     public int damageAmount = 10;
     public float damageCooldown = 1f;
 
+    [SerializeField] private AudioClip hitClip;
+    [SerializeField] private float hitVolume = 1f;
+
     private float lastDamageTime = -999f;
     private PlayerDamageReceiver currentReceiver;
+
+    private static AudioClip s_defaultHitClip;
+    private static AudioSource s_hitAudioSource;
+    private static float s_lastHitSoundTime = -999f;
+
+    private void Awake()
+    {
+        if (hitClip != null && s_defaultHitClip == null)
+            s_defaultHitClip = hitClip;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (hitClip == null)
+            hitClip = AssetDatabase.LoadAssetAtPath<AudioClip>(DefaultHitClipPath);
+    }
+#endif
 
     private void OnTriggerEnter(Collider other)
     {
@@ -57,5 +83,53 @@ public class DamageObject : MonoBehaviour
             ? transform.parent.name + " / " + gameObject.name
             : gameObject.name;
         currentReceiver.ReceiveDamage(damageAmount, source);
+        PlayHitSound();
+    }
+
+    private void PlayHitSound()
+    {
+        if (Time.time - s_lastHitSoundTime < damageCooldown)
+            return;
+
+        AudioClip clip = hitClip != null ? hitClip : GetDefaultHitClip();
+        if (clip == null)
+            return;
+
+        AudioSource source = GetSharedHitAudioSource();
+        if (source == null)
+            return;
+
+        float volume = hitVolume;
+        if (SettingsManager.Instance != null)
+            volume *= SettingsManager.Instance.uiVolume;
+
+        s_lastHitSoundTime = Time.time;
+        source.PlayOneShot(clip, volume);
+    }
+
+    private static AudioClip GetDefaultHitClip()
+    {
+        if (s_defaultHitClip != null)
+            return s_defaultHitClip;
+
+#if UNITY_EDITOR
+        s_defaultHitClip = AssetDatabase.LoadAssetAtPath<AudioClip>(DefaultHitClipPath);
+#endif
+        return s_defaultHitClip;
+    }
+
+    private static AudioSource GetSharedHitAudioSource()
+    {
+        if (s_hitAudioSource != null)
+            return s_hitAudioSource;
+
+        var audioObject = new GameObject("ObstacleHitAudio");
+        DontDestroyOnLoad(audioObject);
+
+        s_hitAudioSource = audioObject.AddComponent<AudioSource>();
+        s_hitAudioSource.playOnAwake = false;
+        s_hitAudioSource.spatialBlend = 0f;
+
+        return s_hitAudioSource;
     }
 }
